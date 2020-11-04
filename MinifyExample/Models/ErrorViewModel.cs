28372ede58e1712c56593219eb439f42683a2732
Doug Riddle <DRiddle@jbu.edu>
2020-11-04 12:55:52 -0600

    Add project files.

---
+++ MinifyExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MinifyExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
